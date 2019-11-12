ff065d0006f27db3e257928ee45b8f4493ed0eaf
Acaree <acampamar@gmail.com>
2019-11-12 11:37:38 +0100

    FIrst steps of client

--- Multiplayer Game/ReplicationManagerClient.cpp
+++ Multiplayer Game/ReplicationManagerClient.cpp
@@ -1,2 +1,43 @@
 #include "Networks.h"
+#include "ReplicationManagerServer.h"
+#include "ModuleGameObject.h"
 
+void ReplicationManagerClient::Read(const InputMemoryStream &packet)
+{
+	ReplicationCommand replication_packet;
+	
+	packet >> replication_packet.networkId;
+	packet >> replication_packet.action;
+
+	switch (replication_packet.action)
+	{
+	case ReplicationAction::Create:
+
+		GameObject go;
+		packet >> go.position.x;
+		packet >> go.position.y;
+		packet >> go.size.x;
+		packet >> go.size.y;
+		packet >> go.angle;
+		packet >> go.tag;
+
+		//instantiate and add it to the list
+
+		break;
+
+	case ReplicationAction::Update:
+
+		packet >> go.position.x;
+		packet >> go.position.y;
+		packet >> go.angle;
+
+
+
+		break;
+
+	case ReplicationAction::Destroy:
+
+		break;
+
+	
+}
--- Multiplayer Game/ReplicationManagerClient.h
+++ Multiplayer Game/ReplicationManagerClient.h
@@ -1,2 +1,19 @@
-#pragma once
+#ifndef _REPLICATIONMANAGERCLIENT_
+#define _REPLICATIONMANAGERCLIENT_
+
+#include "ReplicationManagerServer.h"
+
+enum class ReplicationAction;
+struct ClientProxy;
+struct ReplicationCommand;
+
+class ReplicationManagerClient {
+
+public:
+
+	void Read(const InputMemoryStream &packet);
+
+};
+
+#endif
 
