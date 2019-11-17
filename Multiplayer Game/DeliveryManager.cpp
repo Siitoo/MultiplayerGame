dbad9ea707dee8f6f00fe3e244a1a054a60509bb
Siitoo <alfonsosanchez1896@gmail.com>
2019-11-17 11:53:41 +0100

    DeliveryManager first iteration

--- Multiplayer Game/DeliveryManager.cpp
+++ Multiplayer Game/DeliveryManager.cpp
@@ -1,2 +1,129 @@
 #include "Networks.h"
+#include "DeliveryManager.h"
+
+//Delivery manager functions
+
+Delivery* DeliveryManager::writeSequenceNumber(OutputMemoryStream& packet) 
+{
+	Delivery delivery;
+
+	delivery.sequenceNumber = nextSequenceNumber++;
+
+	packet << delivery.sequenceNumber;
+
+	if (server)
+	{
+		pendingDeliveries.push_back(delivery);
+		return &delivery;
+	}
+	else
+		return nullptr;
+
+}
+
+bool DeliveryManager::processSequenceNumber(const InputMemoryStream& packet)
+{
+	bool ret = false;
+
+	uint32 sequenceNumber = 0;
+	packet >> sequenceNumber;
+
+	if (sequenceNumber == nextExpectedSequenceNumber)
+	{
+		nextExpectedSequenceNumber = sequenceNumber +1;
+
+		if (!server)
+			sequenceNumberPendingAck.push_back(sequenceNumber);
+	}
+
+	return ret;
+}
+
+bool DeliveryManager::hasSequenceNumbersPendingAck() const
+{
+	return sequenceNumberPendingAck.size() > 0;
+}
+
+void DeliveryManager::writeSequenceNumbersPendingAck(OutputMemoryStream& packet)
+{
+	uint32 start = sequenceNumberPendingAck[0];
+	bool AckSize = sequenceNumberPendingAck.size() > 1;
+	
+	packet << start;
+	packet << AckSize;
+
+	if(AckSize)
+	{
+		uint32 range = sequenceNumberPendingAck.size();
+		packet << range - 1;
+	}
+
+	clear();
+
+}
+
+void DeliveryManager::processAckdSequenceNumbers(const InputMemoryStream& packet)
+{
+	uint32 start;
+	packet >> start;
+	bool AckSize;
+	packet >> AckSize;
+	int32 range = 1;
+
+	if (AckSize)
+	{
+		packet >> range;
+	}
+
+	clear(start,range);
+}
+
+void DeliveryManager::processTimeOutPackets()
+{
+	if (pendingDeliveries.size() > 0)
+	{
+		for (std::vector<Delivery>::iterator it = pendingDeliveries.begin(); it != pendingDeliveries.end(); ++it)
+		{
+			if (it->dispatchTime > timeoutAck)
+			{
+				pendingDeliveries.erase(it);
+			}
+			else
+			{
+				it->dispatchTime += Time.deltaTime;
+			}
+		}
+	}
+
+
+}
+
+void DeliveryManager::clear(uint32 start,uint32 size)
+{
+	if (server)
+	{
+		for (std::vector<Delivery>::iterator it = pendingDeliveries.begin(); it != pendingDeliveries.end(); ++it)
+		{
+			if (it->sequenceNumber == start)
+			{
+				for (uint32 i = 0; i < size; ++i)
+				{
+					//(*it)->deliveryDelegate->onDeliverySuccess();
+
+					//Sito i don't know if need increment or not it++
+					pendingDeliveries.erase(it);
+					it++;
+				}
+				pendingDeliveries.shrink_to_fit();
+				break;
+			}
+		}
+
+
+	}
+	else
+	{
+		sequenceNumberPendingAck.clear();
+	}
+}
 
--- Multiplayer Game/DeliveryManager.h
+++ Multiplayer Game/DeliveryManager.h
@@ -1,2 +1,55 @@
-#pragma once
+#ifndef _DELIVERYMANAGER_
+#define _DELIVERYMANAGER_
+
+class DeliveryDelegate;
+struct Delivery
+{
+	uint32 sequenceNumber = 0;
+	double dispatchTime = 0.0;
+	DeliveryDelegate *deliveryDelegate = nullptr;
+};
+
+class DeliveryManager
+{
+public:
+
+	Delivery* writeSequenceNumber(OutputMemoryStream& packet);
+
+	bool processSequenceNumber(const InputMemoryStream& packet);
+
+	bool hasSequenceNumbersPendingAck() const;
+	void writeSequenceNumbersPendingAck(OutputMemoryStream& packet);
+
+	void processAckdSequenceNumbers(const InputMemoryStream& packet);
+	void processTimeOutPackets();
+
+	void clear(uint32 start = 0, uint32 size = 0);
+
+private:
+	uint32 nextSequenceNumber = 0;
+	std::vector<Delivery> pendingDeliveries;
+
+	//Detect if i'm are a server or client
+	bool server = false;
+
+	uint32 nextExpectedSequenceNumber = 0;
+	std::vector<uint32> sequenceNumberPendingAck;
+
+	float timeoutAck = 0.8f;
+
+};
+
+
+
+class DeliveryDelegate
+{
+public:
+	virtual void onDeliverySuccess(DeliveryManager *deliveryManager) = 0;
+	virtual void onDeliveryFailure(DeliveryManager *deliveryManager) = 0;
+};
+
+
+
+
+#endif // !_DELIVERYMANAGER_
 
